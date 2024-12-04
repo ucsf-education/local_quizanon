b105025fa4419f9254faac1c86d7bac63e59aa97
Oscar Nadjar <oscar.nadjar@gmail.com>
2024-12-04 12:53:29 +0100

    DEF-3389: Fixing privacy provider test for local_quizanon

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024040900; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024120400; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022112800; // Requires this Moodle version.
 $plugin->component = 'local_quizanon'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
