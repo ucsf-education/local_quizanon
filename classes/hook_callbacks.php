fd615cf047fb9931c106f11a82fed0132339fe64
lbailey-ucsf <leon.bailey@ucsf.edu>
2024-12-27 09:31:27 -0800

    Fix Moodle Code Checker and Behat feature errors.

--- classes/hook_callbacks.php
+++ classes/hook_callbacks.php
@@ -1,10 +1,31 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hook callbacks for the Quiz Anonymization plugin
+ *
+ * @package    local_quizanon
+ * @copyright  2024 UCSF Education IT
+ * @author     Leon U. Bailey <leon.bailey@ucsf.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace local_quizanon;
 
-use \core\hook\output\before_standard_top_of_body_html_generation;
-
-defined('MOODLE_INTERNAL') || die;
+use core\hook\output\before_standard_top_of_body_html_generation;
 
 class hook_callbacks {
     /**
--- db/hooks.php
+++ db/hooks.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * Hooks configuration for the Quiz Anonymization plugin
+ *
+ * @package    local_quizanon
+ * @copyright  2024 UCSF Education IT
+ * @author     Leon U. Bailey <leon.bailey@ucsf.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 $callbacks = [
     [
         'hook' => \core\hook\output\before_standard_top_of_body_html_generation::class,
-        'callback' => [\local_quizanon\hook_callbacks::class,'before_standard_top_of_body_html'],
-        'priority' => 200,
-    ],
+        'callback' => '\local_quizanon\hook_callbacks::before_standard_top_of_body_html',
+        'priority' => 0,
+    ], 
 ];
