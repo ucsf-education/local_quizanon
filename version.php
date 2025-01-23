bcede815d1e98c1a26dc791730e88f186413ed71
lbailey-ucsf <leon.bailey@ucsf.edu>
2025-01-23 12:19:17 -0800

    New stable branch with version bump, requiring minimum M4.4.0, and supporting the Moodle 404 branch.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024120402; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022112800; // Requires this Moodle version.
+$plugin->version   = 2024120403; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2024042200; // Requires this Moodle version.
 $plugin->component = 'local_quizanon'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
-$plugin->supported = [401, 404];
+$plugin->supported = [404, 404];
