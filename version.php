632feb95eed4117dd0e609362b846be9a1c83562
lbailey-ucsf <leon.bailey@ucsf.edu>
2025-04-01 14:39:40 -0700

    version bump and moodle-branch bump to 405

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024120403; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025040100; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024042200; // Requires this Moodle version.
 $plugin->component = 'local_quizanon'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
-$plugin->supported = [404, 404];
+$plugin->release = '1.0.1';
+$plugin->supported = [405, 405];
